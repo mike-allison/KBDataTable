26b6473a1ab7380e5459e002a5e43ea0e49f2102
mikeallison <dj.mikeallison@gmail.com>
2014-01-09 15:22:33 -0600

    Update KBDataTable-1.0.0.js

--- KBDataTable-1.0.0.js
+++ KBDataTable-1.0.0.js
@@ -20,7 +20,7 @@
     defaults: {
       "searchText": "",
       "columns": [],
-      "rows": [],
+      "rows": new Backbone.Collection,
       "currentPage": 0,
       "pageSize": 20,
       "selectedColumn": 0,
@@ -46,7 +46,7 @@
       this.model = model;
       this.searchText = kb.observable(this.model, 'searchText');
       this.columns = kb.observable(this.model, 'columns');
-      this.rows = kb.observable(this.model, 'rows');
+      this.rows = new kb.CollectionObservable(this.model.attributes.rows);
       this.currentPage = kb.observable(this.model, 'currentPage');
       this.pageSize = kb.observable(this.model, 'pageSize');
       this.selectedColumn = kb.observable(this.model, 'selectedColumn');
@@ -72,7 +72,7 @@
           return _this.rows();
         } else {
           return ko.utils.arrayFilter(_this.rows(), function(item) {
-            return item[_this.selectedColumn()].toString().toLowerCase().indexOf(filter) > -1;
+            return item.model().get([_this.selectedColumn()]).toString().toLowerCase().indexOf(filter) > -1;
           });
         }
       });
@@ -86,13 +86,13 @@
       this.pageCount = ko.computed(function() {
         return Math.ceil(_this.filteredRows().length / _this.pageSize());
       });
-      this.nextFn = this.model.get('nextFn');
-      this.prevFn = this.model.get('prevFn');
-      this.searchFn = this.model.get('searchFn');
-      this.sortFn = this.model.get('sortFn');
-      this.lastFn = this.model.get('lastFn');
-      this.firstFn = this.model.get('firstFn');
-      this.selectFn = this.model.get('selectFn');
+      this.nextFn = this.model.get('nextFn' != null ? 'nextFn' : null);
+      this.prevFn = this.model.get('prevFn' != null ? 'prevFn' : null);
+      this.searchFn = this.model.get('searchFn' != null ? 'searchFn' : null);
+      this.sortFn = this.model.get('sortFn' != null ? 'sortFn' : null);
+      this.lastFn = this.model.get('lastFn' != null ? 'lastFn' : null);
+      this.firstFn = this.model.get('firstFn' != null ? 'firstFn' : null);
+      this.selectFn = this.model.get('selectFn' != null ? 'selectFn' : null);
       if (typeof jQuery.fn.jTableScroll === "function") {
         jQuery(function() {
           return jQuery('.jTableScroll').jTableScroll({
@@ -139,7 +139,7 @@
       if (typeof this.searchFn === "function") {
         this.searchFn();
       }
-      this.filter(this.throttleSearch()());
+      this.filter(this.searchText());
       return this.currentPage(0);
     };
 
@@ -147,7 +147,7 @@
       var _ref,
         _this = this;
 
-      if (typeof this.sortFn === "function") {
+      if (this.sortFn === "function") {
         return this.sortFn(index);
       } else {
         if (!this.sortDir[index]) {
@@ -155,17 +155,17 @@
         }
         this.rows.sort(function(left, right) {
           if (_this.sortDir[index] === "A") {
-            if (left[_this.columns()[index]] === right[_this.columns()[index]]) {
+            if (left.model().get(_this.columns()[index]) === right.model().get(_this.columns()[index])) {
               return 0;
-            } else if (left[_this.columns()[index]] < right[_this.columns()[index]]) {
+            } else if (left.model().get(_this.columns()[index]) < right.model().get(_this.columns()[index])) {
               return -1;
             } else {
               return 1;
             }
           } else {
-            if (left[_this.columns()[index]] === right[_this.columns()[index]]) {
+            if (left.model().get(_this.columns()[index]) === right.model().get(_this.columns()[index])) {
               return 0;
-            } else if (left[_this.columns()[index]] > right[_this.columns()[index]]) {
+            } else if (left.model().get(_this.columns()[index]) > right.model().get(_this.columns()[index])) {
               return -1;
             } else {
               return 1;
@@ -179,7 +179,7 @@
     };
 
     KBDataTableViewModel.prototype.selectRow = function(data) {
-      this.selectedRow(data);
+      this.selectedRow(data.model().cid);
       return typeof this.selectFn === "function" ? this.selectFn() : void 0;
     };
 
